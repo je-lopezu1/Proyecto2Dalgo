dd50724407abdb1ed8040e38ef9792085a01ce6e
Lele <n.londonoc@uniandes.edu.co>
2023-05-04 22:28:50 -0500

    Arreglo 2.0

--- ProyectoDalgoP2/Graph.java
+++ ProyectoDalgoP2/Graph.java
@@ -1,60 +1,64 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Graph {
     int vertices;
-    int edges;
-    Edge[] ad;
+    List<List<Integer>> ad;
+    int[] components;
 
-    class Edge {
-        int src, dst;
+    public static void addEdge(int from, int to, List<List<Integer>> g) {
+        g.get(from).add(to);
+        g.get(to).add(from);
     }
 
-    Graph(int v, int e) {
+    Graph(int v) {
         vertices = v;
-        edges = e;
-        ad = new Edge[e];
-        for (int i = 0; i < e; i++) {
-            ad[i] = new Edge();
+        ad = new ArrayList<>();
+        components = new int[v];
+        for (int i = 0; i < v; i++) {
+            ad.add(i, new LinkedList<>());
+            components[i] = -1;
         }
     }
 
-    public int find(int parent[], int i) {
-        if (parent[i] == i) {
-            return i;
-        }
-        return find(parent, parent[i]);
-    }
-
-    public void union (int parent[], int x, int y) {
-        parent[x] = y;
-    }
-
-    public static boolean equalSets (Graph coax, Graph optic) {
-        int[] coaxSet = new int[coax.vertices];
-        int[] opticSet = new int[optic.vertices];
-
-        for (int i = 0; i < coax.vertices; i++) {
-            coaxSet[i] = i;
-            opticSet[i] = i;
-        }
-
-        for (int i = 0; i < coax.edges; i++) {
-            int x = coax.find(coaxSet, coax.ad[i].src);
-            int y = coax.find(coaxSet, coax.ad[i].dst);
-
-            coax.union(coaxSet, x, y);
-        }
-
-        for (int i = 0; i < optic.edges; i++) {
-            int x = optic.find(opticSet, optic.ad[i].src);
-            int y = optic.find(opticSet, optic.ad[i].dst);
-
-            optic.union(opticSet, x, y);
-        }
-
-        for (int i = 0; i < coaxSet.length; i++) {
-            if (coaxSet[i] == opticSet[i]) {
-                return true;
+    public void union(int from, int to) {
+        if (components[to] == -1) {
+            components[to] = components[from];
+        } else if (components[from] < components[to]) {
+            for (int i = 0; i < vertices; i++) {
+                if (components[i] == components[to]) {
+                    components[i] = components[from];
+                }
+            }
+        } else {
+            for (int i = 0; i < vertices; i++) {
+                if (components[i] == components[from]) {
+                    components[i] = components[to];
+                }
             }
         }
-        return false;
+    }
+
+    public static boolean equalSets(Graph coax, Graph optic, int src, int dst, int type) {
+        if (type == 1) {
+            if (coax.components[src] == -1) {
+                coax.components[src] = src;
+            }
+            coax.union(src, dst);
+        } else {
+            if (optic.components[src] == -1) {
+                optic.components[src] = src;
+            }
+            optic.union(src, dst);
+        }
+
+        for (int i = 0; i < coax.vertices; i++) {
+            if (coax.components[i] != optic.components[i]) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
