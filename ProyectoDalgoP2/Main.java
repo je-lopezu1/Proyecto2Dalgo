e912406bc1bf8fb37d6e5783220d7c0e907ae6a8
Maria <m.lizarazop@uniandes.edu.co>
2023-05-04 22:39:46 -0500

    input :>

--- ProyectoDalgoP2/Main.java
+++ ProyectoDalgoP2/Main.java
@@ -1,61 +1,53 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
 		try {
-			File file = new File("prueba.txt");
-			BufferedReader br = new BufferedReader(new FileReader(file));
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			int size = Integer.parseInt(br.readLine());
-			// Map<Integer, ArrayList<ArrayList<Integer>>> casos = new HashMap<>();
+
 			for (int i = 0; i < size; i++) {
 				String[] line = br.readLine().split(" ");
 				int n = Integer.parseInt(line[0]);
 				int m = Integer.parseInt(line[1]);
-				ArrayList<ArrayList<ArrayList<Integer>>> caso = Main.crearListasAdyacencia(n);
+
+				Graph coaxial = new Graph(n);
+				Graph opti = new Graph(n);
+
 				for (int j = 0; j < m; j++) {
 					String[] ejes = br.readLine().split(" ");
-					if (ejes[2].equals("1")) {
-						caso.get(0).get(Integer.parseInt(ejes[0]) - 1).add(Integer.parseInt(ejes[1]) - 1);
-						caso.get(0).get(Integer.parseInt(ejes[1]) - 1).add(Integer.parseInt(ejes[0]) - 1);
-					} else {
-						caso.get(1).get(Integer.parseInt(ejes[0]) - 1).add(Integer.parseInt(ejes[1]) - 1);
-						caso.get(1).get(Integer.parseInt(ejes[1]) - 1).add(Integer.parseInt(ejes[0]) - 1);
-					}
-					Dfs dfsOptica = new Dfs();
-					Dfs dfsCoaxial = new Dfs();
-					dfsOptica.findConnectedComponents(caso.get(0));
-					dfsCoaxial.findConnectedComponents(caso.get(1));
-					System.out.println(Arrays.toString(dfsOptica.getComponents()));
-					System.out.println(Arrays.toString(dfsCoaxial.getComponents()));
 
-					System.out.println(caso);
+					if (ejes[2].equals("1")) {
+						Graph.addEdge(Integer.parseInt(ejes[0]) - 1, Integer.parseInt(ejes[1]) - 1, coaxial.ad);
+					} else {
+						Graph.addEdge(Integer.parseInt(ejes[0]) - 1, Integer.parseInt(ejes[1]) - 1, opti.ad);
+
+					}
+					boolean b = Graph.equalSets(coaxial, opti, Integer.parseInt(ejes[0]) - 1,
+							Integer.parseInt(ejes[1]) - 1, Integer.parseInt(ejes[2]));
+					if (b) {
+						System.out.print(1);
+					} else {
+						System.out.print(0);
+					}
+					if (j != m - 1) {
+						System.out.print(" ");
+					}
+
+				}
+				if (i != size - 1) {
+					System.out.println();
 				}
 
 			}
 			br.close();
-		} catch (Exception e) {
-			System.out.println("Error al abrir el archivo");
+		} catch (IOException e) {
+			System.out.println("Error al leer los datos de entrada");
 		}
 	}
 
-	private static ArrayList<ArrayList<ArrayList<Integer>>> crearListasAdyacencia(int n) {
-		ArrayList<ArrayList<ArrayList<Integer>>> caso = new ArrayList<>(n);
-		ArrayList<ArrayList<Integer>> coaxial = new ArrayList<>(n);
-		ArrayList<ArrayList<Integer>> optica = new ArrayList<>(n);
-		for (int i = 0; i < n; i++) {
-			coaxial.add(new ArrayList<Integer>());
-			optica.add(new ArrayList<Integer>());
-		}
-		caso.add(optica);
-		caso.add(coaxial);
-		return caso;
-	}
 }
